c03447b5222274b08399de41159b97c14e78bae8
Marc Noirot <marc.noirot@gmail.com>
2011-09-30 17:56:31 +0200

    Started work on better referencing check message codes.

--- src/check.c
+++ src/check.c
@@ -208,57 +208,57 @@ int check_flv_file(const flvmeta_opts * opts) {
     /* check signature */
     result = flv_read_header(flv_in, &header);
     if (result == FLV_ERROR_EOF) {
-        print_fatal("F11001", 0, "unexpected end of file in header");
+        print_fatal(FATAL_HEADER_EOF, 0, "unexpected end of file in header");
         goto end;
     }
     else if (result == FLV_ERROR_NO_FLV) {
-        print_fatal("F11002", 0, "FLV signature not found in header");
+        print_fatal(FATAL_HEADER_NO_SIGNATURE, 0, "FLV signature not found in header");
         goto end;
     }
 
     /* version */
     if (header.version != FLV_VERSION) {
         sprintf(message, "header version should be 1, %d found instead", header.version);
-        print_error("E11003", 3, message);
+        print_error(ERROR_HEADER_BAD_VERSION, 3, message);
     }
 
     /* video and audio flags */
     if (!flv_header_has_audio(header) && !flv_header_has_video(header)) {
-        print_error("E11004", 4, "header signals the file does not contain video tags or audio tags");
+        print_error(ERROR_HEADER_NO_STREAMS, 4, "header signals the file does not contain video tags or audio tags");
     }
     else if (!flv_header_has_audio(header)) {
-        print_info("I11005", 4, "header signals the file does not contain audio tags");
+        print_info(INFO_HEADER_NO_AUDIO, 4, "header signals the file does not contain audio tags");
     }
     else if (!flv_header_has_video(header)) {
-        print_warning("W11006", 4, "header signals the file does not contain video tags");
+        print_warning(WARNING_HEADER_NO_VIDEO, 4, "header signals the file does not contain video tags");
     }
 
     /* reserved flags */
     if (header.flags & 0xFA) {
-        print_error("E11007", 4, "header reserved flags are not zero");
+        print_error(ERROR_HEADER_BAD_RESERVED_FLAGS, 4, "header reserved flags are not zero");
     }
 
     /* offset */
     if (flv_header_get_offset(header) != 9) {
         sprintf(message, "header offset should be 9, %d found instead", flv_header_get_offset(header));
-        print_error("E11008", 5, message);
+        print_error(ERROR_HEADER_BAD_OFFSET, 5, message);
     }
 
     /** check first previous tag size **/
 
     result = flv_read_prev_tag_size(flv_in, &prev_tag_size);
     if (result == FLV_ERROR_EOF) {
-        print_fatal("F12009", 9, "unexpected end of file in previous tag size");
+        print_fatal(FATAL_PREV_TAG_SIZE_EOF, 9, "unexpected end of file in previous tag size");
         goto end;
     }
     else if (prev_tag_size != 0) {
         sprintf(message, "first previous tag size should be 0, %d found instead", prev_tag_size);
-        print_error("E12010", 9, message);
+        print_error(ERROR_PREV_TAG_SIZE_BAD_FIRST, 9, message);
     }
 
     /* we reached the end of file: no tags in file */
     if (flv_get_offset(flv_in) == file_stats.st_size) {
-        print_fatal("F10011", 13, "file does not contain tags");
+        print_fatal(FATAL_GENERAL_NO_TAG, 13, "file does not contain tags");
         goto end;
     }
 
@@ -271,7 +271,7 @@ int check_flv_file(const flvmeta_opts * opts) {
 
         result = flv_read_tag(flv_in, &tag);
         if (result != FLV_OK) {
-            print_fatal("F20012", flv_get_offset(flv_in), "unexpected end of file in tag");
+            print_fatal(FATAL_TAG_EOF, flv_get_offset(flv_in), "unexpected end of file in tag");
             goto end;
         }
 
@@ -288,19 +288,19 @@ int check_flv_file(const flvmeta_opts * opts) {
             && tag.type != FLV_TAG_TYPE_META
         ) {
             sprintf(message, "unknown tag type %hhd", tag.type);
-            print_error("E30013", offset, message);
+            print_error(ERROR_TAG_TYPE_UNKNOWN, offset, message);
         }
 
         /* check consistency with global header */
         if (!have_video && tag.type == FLV_TAG_TYPE_VIDEO) {
             if (!flv_header_has_video(header)) {
-                print_warning("W11014", offset, "video tag found despite header signaling the file contains no video");
+                print_warning(WARNING_HEADER_UNEXPECTED_VIDEO, offset, "video tag found despite header signaling the file contains no video");
             }
             have_video = 1;
         }
         if (!have_audio && tag.type == FLV_TAG_TYPE_AUDIO) {
             if (!flv_header_has_audio(header)) {
-                print_warning("W11015", offset, "audio tag found despite header signaling the file contains no audio");
+                print_warning(WARNING_HEADER_UNEXPECTED_AUDIO, offset, "audio tag found despite header signaling the file contains no audio");
             }
             have_audio = 1;
         }
@@ -308,15 +308,15 @@ int check_flv_file(const flvmeta_opts * opts) {
         /* check body length */
         if (body_length > (file_stats.st_size - flv_get_offset(flv_in))) {
             sprintf(message, "tag body length (%d bytes) exceeds file size", body_length);
-            print_fatal("F20016", offset + 1, message);
+            print_fatal(FATAL_TAG_BODY_LENGTH_OVERFLOW, offset + 1, message);
             goto end;
         }
         else if (body_length > MAX_ACCEPTABLE_TAG_BODY_LENGTH) {
             sprintf(message, "tag body length (%d bytes) is abnormally large", body_length);
-            print_warning("W20017", offset + 1, message);
+            print_warning(WARNING_TAG_BODY_LENGTH_LARGE, offset + 1, message);
         }
         else if (body_length == 0) {
-            print_warning("W20018", offset + 1, "tag body length is zero");
+            print_warning(WARNING_TAG_BODY_LENGTH_ZERO, offset + 1, "tag body length is zero");
         }
 
         /** check timestamp **/
@@ -325,14 +325,14 @@ int check_flv_file(const flvmeta_opts * opts) {
         /* check whether first timestamp is zero */
         if (tag_number == 1 && timestamp != 0) {
             sprintf(message, "first timestamp should be zero, %d found instead", timestamp);
-            print_error("E40019", offset + 4, message);
+            print_error(ERROR_TIMESTAMP_FIRST_NON_ZERO, offset + 4, message);
         }
 
         /* check whether timestamps decrease in a given stream */
         if (tag.type == FLV_TAG_TYPE_AUDIO) {
             if (last_audio_timestamp > timestamp) {
                 sprintf(message, "audio tag timestamps are decreasing from %d to %d", last_audio_timestamp, timestamp);
-                print_error("E40020", offset + 4, message);
+                print_error(ERROR_TIMESTAMP_AUDIO_DECREASE, offset + 4, message);
             }
             last_audio_timestamp = timestamp;
             decr_timestamp_signaled = 1;
@@ -340,7 +340,7 @@ int check_flv_file(const flvmeta_opts * opts) {
         if (tag.type == FLV_TAG_TYPE_VIDEO) {
             if (last_video_timestamp > timestamp) {
                 sprintf(message, "video tag timestamps are decreasing from %d to %d", last_video_timestamp, timestamp);
-                print_error("E40021", offset + 4, message);
+                print_error(ERROR_TIMESTAMP_VIDEO_DECREASE, offset + 4, message);
             }
             last_video_timestamp = timestamp;
             decr_timestamp_signaled = 1;
@@ -348,13 +348,13 @@ int check_flv_file(const flvmeta_opts * opts) {
 
         /* check for overflow error */
         if (last_timestamp > timestamp && last_timestamp - timestamp > 0xF00000) {
-            print_error("E40022", offset + 4, "extended bits not used after timestamp overflow");
+            print_error(ERROR_TIMESTAMP_OVERFLOW, offset + 4, "extended bits not used after timestamp overflow");
         }
 
         /* check whether timestamps decrease globally */
         else if (!decr_timestamp_signaled && last_timestamp > timestamp && last_timestamp - timestamp >= 1000) {
             sprintf(message, "timestamps are decreasing from %d to %d", last_timestamp, timestamp);
-            print_error("E40023", offset + 4, message);
+            print_error(ERROR_TIMESTAMP_DECREASE, offset + 4, message);
         }
 
         last_timestamp = timestamp;
@@ -363,14 +363,14 @@ int check_flv_file(const flvmeta_opts * opts) {
         if (have_video && have_audio && !have_desync && labs(last_video_timestamp - last_audio_timestamp) >= 1000) {
             sprintf(message, "audio and video streams are desynchronized by %ld ms",
                 labs(last_video_timestamp - last_audio_timestamp));
-            print_warning("W40024", offset + 4, message);
+            print_warning(WARNING_TIMESTAMP_DESYNC, offset + 4, message);
             have_desync = 1; /* do not repeat */
         }
 
         /** stream id must be zero **/
         if (stream_id != 0) {
             sprintf(message, "tag stream id must be zero, %d found instead", stream_id);
-            print_error("E20025", offset + 8, message);
+            print_error(ERROR_TAG_STREAM_ID_NON_ZERO, offset + 8, message);
         }
 
         /* check tag body contents only if not empty */
@@ -383,20 +383,20 @@ int check_flv_file(const flvmeta_opts * opts) {
 
                 result = flv_read_audio_tag(flv_in, &at);
                 if (result == FLV_ERROR_EOF) {
-                    print_fatal("F20012", offset + 11, "unexpected end of file in tag");
+                    print_fatal(FATAL_TAG_EOF, offset + 11, "unexpected end of file in tag");
                     goto end;
                 }
 
                 /* check whether the format varies between tags */
                 if (have_prev_audio_tag && prev_audio_tag != at) {
-                    print_warning("W51026", offset + 11, "audio format changed since last tag");
+                    print_warning(WARNING_AUDIO_FORMAT_CHANGED, offset + 11, "audio format changed since last tag");
                 }
 
                 /* check format */
                 audio_format = flv_audio_tag_sound_format(at);
                 if (audio_format == 12 || audio_format == 13) {
                     sprintf(message, "unknown audio format %d", audio_format);
-                    print_warning("W51027", offset + 11, message);
+                    print_warning(WARNING_AUDIO_CODEC_UNKNOWN, offset + 11, message);
                 }
                 else if (audio_format == FLV_AUDIO_TAG_SOUND_FORMAT_G711_A
                     || audio_format == FLV_AUDIO_TAG_SOUND_FORMAT_G711_MU
@@ -405,14 +405,14 @@ int check_flv_file(const flvmeta_opts * opts) {
                     || audio_format == FLV_AUDIO_TAG_SOUND_FORMAT_DEVICE_SPECIFIC
                 ) {
                     sprintf(message, "audio format %d is reserved for internal use", audio_format);
-                    print_warning("W51028", offset + 11, message);
+                    print_warning(WARNING_AUDIO_CODEC_RESERVED, offset + 11, message);
                 }
 
                 /* check consistency, see flash video spec */
                 if (flv_audio_tag_sound_rate(at) != FLV_AUDIO_TAG_SOUND_RATE_44
                     && audio_format == FLV_AUDIO_TAG_SOUND_FORMAT_AAC
                 ) {
-                    print_warning("W51029", offset + 11, "audio data in AAC format should have a 44KHz rate, field will be ignored");
+                    print_warning(WARNING_AUDIO_CODEC_AAC_BAD, offset + 11, "audio data in AAC format should have a 44KHz rate, field will be ignored");
                 }
 
                 if (flv_audio_tag_sound_type(at) == FLV_AUDIO_TAG_SOUND_TYPE_STEREO
@@ -420,7 +420,7 @@ int check_flv_file(const flvmeta_opts * opts) {
                         || audio_format == FLV_AUDIO_TAG_SOUND_FORMAT_NELLYMOSER_16_MONO
                         || audio_format == FLV_AUDIO_TAG_SOUND_FORMAT_NELLYMOSER_8_MONO)
                 ) {
-                    print_warning("W51030", offset + 11, "audio data in Nellymoser format cannot be stereo, field will be ignored");
+                    print_warning(WARNING_AUDIO_CODEC_NELLYMOSER_BAD, offset + 11, "audio data in Nellymoser format cannot be stereo, field will be ignored");
                 }
 
                 else if (flv_audio_tag_sound_type(at) == FLV_AUDIO_TAG_SOUND_TYPE_MONO
--- src/check.h
+++ src/check.h
@@ -26,6 +26,61 @@
 
 #include "flvmeta.h"
 
+/* message level codes */
+#define LEVEL_FATAL     "F"
+#define LEVEL_ERROR     "E"
+#define LEVEL_WARNING   "W"
+#define LEVEL_INFO      "I"
+
+/* message topic codes */
+#define TOPIC_GENERAL_FORMAT    "10"
+#define TOPIC_HEADER            "11"
+#define TOPIC_PREV_TAG_SIZE     "12"
+#define TOPIC_TAG_FORMAT        "20"
+#define TOPIC_TAG_TYPES         "30"
+#define TOPIC_TIMESTAMPS        "40"
+#define TOPIC_AUDIO_DATA        "50"
+#define TOPIC_AUDIO_CODECS      "51"
+#define TOPIC_VIDEO_DATA        "60"
+#define TOPIC_VIDEO_CODECS      "61"
+#define TOPIC_METADATA          "70"
+#define TOPIC_AMF_DATA          "80"
+#define TOPIC_KEYFRAMES         "81"
+#define TOPIC_CUE_POINTS        "82"
+
+/* info, warning, error codes, sorted by unique identifier */
+#define FATAL_HEADER_EOF                    LEVEL_FATAL     TOPIC_HEADER            "001"
+#define FATAL_HEADER_NO_SIGNATURE           LEVEL_FATAL     TOPIC_HEADER            "002"
+#define ERROR_HEADER_BAD_VERSION            LEVEL_ERROR     TOPIC_HEADER            "003"
+#define ERROR_HEADER_NO_STREAMS             LEVEL_ERROR     TOPIC_HEADER            "004"
+#define INFO_HEADER_NO_AUDIO                LEVEL_INFO      TOPIC_HEADER            "005"
+#define WARNING_HEADER_NO_VIDEO             LEVEL_WARNING   TOPIC_HEADER            "006"
+#define ERROR_HEADER_BAD_RESERVED_FLAGS     LEVEL_ERROR     TOPIC_HEADER            "007"
+#define ERROR_HEADER_BAD_OFFSET             LEVEL_ERROR     TOPIC_HEADER            "008"
+#define FATAL_PREV_TAG_SIZE_EOF             LEVEL_FATAL     TOPIC_PREV_TAG_SIZE     "009"
+#define ERROR_PREV_TAG_SIZE_BAD_FIRST       LEVEL_ERROR     TOPIC_PREV_TAG_SIZE     "010"
+#define FATAL_GENERAL_NO_TAG                LEVEL_FATAL     TOPIC_GENERAL_FORMAT    "011"
+#define FATAL_TAG_EOF                       LEVEL_FATAL     TOPIC_TAG_FORMAT        "012"
+#define ERROR_TAG_TYPE_UNKNOWN              LEVEL_ERROR     TOPIC_TAG_TYPES         "013"
+#define WARNING_HEADER_UNEXPECTED_VIDEO     LEVEL_WARNING   TOPIC_HEADER            "014"
+#define WARNING_HEADER_UNEXPECTED_AUDIO     LEVEL_WARNING   TOPIC_HEADER            "015"
+#define FATAL_TAG_BODY_LENGTH_OVERFLOW      LEVEL_FATAL     TOPIC_TAG_FORMAT        "016"
+#define WARNING_TAG_BODY_LENGTH_LARGE       LEVEL_WARNING   TOPIC_TAG_FORMAT        "017"
+#define WARNING_TAG_BODY_LENGTH_ZERO        LEVEL_WARNING   TOPIC_TAG_FORMAT        "018"
+#define ERROR_TIMESTAMP_FIRST_NON_ZERO      LEVEL_ERROR     TOPIC_TIMESTAMPS        "019"
+#define ERROR_TIMESTAMP_AUDIO_DECREASE      LEVEL_ERROR     TOPIC_TIMESTAMPS        "020"
+#define ERROR_TIMESTAMP_VIDEO_DECREASE      LEVEL_ERROR     TOPIC_TIMESTAMPS        "021"
+#define ERROR_TIMESTAMP_OVERFLOW            LEVEL_ERROR     TOPIC_TIMESTAMPS        "022"
+#define ERROR_TIMESTAMP_DECREASE            LEVEL_ERROR     TOPIC_TIMESTAMPS        "023"
+#define WARNING_TIMESTAMP_DESYNC            LEVEL_WARNING   TOPIC_TIMESTAMPS        "024"
+#define ERROR_TAG_STREAM_ID_NON_ZERO        LEVEL_ERROR     TOPIC_TAG_FORMAT        "025"
+#define WARNING_AUDIO_FORMAT_CHANGED        LEVEL_WARNING   TOPIC_AUDIO_DATA        "026"
+#define WARNING_AUDIO_CODEC_UNKNOWN         LEVEL_WARNING   TOPIC_AUDIO_CODECS      "027"
+#define WARNING_AUDIO_CODEC_RESERVED        LEVEL_WARNING   TOPIC_AUDIO_CODECS      "028"
+#define WARNING_AUDIO_CODEC_AAC_BAD         LEVEL_WARNING   TOPIC_AUDIO_CODECS      "029"
+#define WARNING_AUDIO_CODEC_NELLYMOSER_BAD  LEVEL_WARNING   TOPIC_AUDIO_CODECS      "030"
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
