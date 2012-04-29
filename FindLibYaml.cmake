78be7d957f651c243a7b1c762c0c4d4a7b5d6e61
Marc Noirot <marc.noirot@gmail.com>
2012-04-29 18:37:44 +0200

    Added an option to link flvmeta to the version of libyaml installed on the system. Cleaned up CMake files.

---
+++ FindLibYaml.cmake
@@ -0,0 +1,13 @@
+# CMake module to search for the libyaml library
+# (library for parsing YAML files)
+# If it's found it sets LIBYAML_FOUND to TRUE
+# and following variables are set:
+#    LIBYAML_INCLUDE_DIR
+#    LIBYAML_LIBRARY
+
+FIND_PATH(LIBYAML_INCLUDE_DIR NAMES yaml.h)
+FIND_LIBRARY(LIBYAML_LIBRARIES NAMES yaml libyaml)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Yaml DEFAULT_MSG LIBYAML_LIBRARIES LIBYAML_INCLUDE_DIR)
+MARK_AS_ADVANCED(LIBYAML_INCLUDE_DIR LIBYAML_LIBRARIES)
