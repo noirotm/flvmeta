2da8d81ad41a467723b6ae1a1df1c0c6e294b443
Marc Noirot <marc.noirot@gmail.com>
2012-05-04 14:44:38 +0200

    Fixed a compilation warning.

--- src/json.c
+++ src/json.c
@@ -22,9 +22,9 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
+#include "json.h"
 #include <stdio.h>
 #include <string.h>
-#include "json.h"
 
 static void json_print_string(const char * str, size_t bytes) {
     size_t i;
