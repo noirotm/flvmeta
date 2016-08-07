29dcdc7c6519dc9d6310bde49b26bce248b1f649
Marc Noirot <marc.noirot@gmail.com>
2016-08-07 16:22:38 +0200

    Add information header to test source files

--- tests/check_flv.c
+++ tests/check_flv.c
@@ -1,3 +1,24 @@
+/*
+    FLVMeta - FLV Metadata Editor
+
+    Copyright (C) 2007-2016 Marc Noirot <marc.noirot AT gmail.com>
+
+    This file is part of FLVMeta.
+
+    FLVMeta is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLVMeta is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLVMeta; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 #include <check.h>
 #include <stdlib.h>
 #include <string.h>
